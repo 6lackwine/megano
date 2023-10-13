08558ff2c6be432ab8c520e47994d8dbc48ccdd9
Самир Багиров <samir_261999@mail.ru>
2023-10-13 11:37:45 +0300

    Добавлена фильтрация в Catalog

--- marketplace/catalog/serializers.py
+++ marketplace/catalog/serializers.py
@@ -31,6 +31,9 @@ class CategoriesSerializers(serializers.ModelSerializer):
         fields = "id", "title", "image", "subcategories"
 
 class CustomPagination(pagination.PageNumberPagination):
+    page_size = 2
+    max_page_size = 1000
+    page_query_param = "page_size"
     def get_paginated_response(self, data):
         return Response({
             'items': data,
--- marketplace/catalog/views.py
+++ marketplace/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import pagination
 
 from rest_framework.generics import GenericAPIView, ListAPIView
@@ -8,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
+from catalog.filters import ProductFilter
 from catalog.models import Categories
 from catalog.serializers import CategoriesSerializers, CustomPagination
 from products.models import Product
@@ -21,11 +23,30 @@ class CategoryAPIView(APIView):
         return Response(serialized.data)
 
 class CatalogAPIView(ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializers
     pagination_class = CustomPagination
-    filterset_fields = [
-        "title",
-        "price",
-        "freeDelivery",
-    ]
+    #filterset_class = ProductFilter
+    filter_backends = (DjangoFilterBackend,)
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        if self.request.query_params:
+            name = self.request.query_params.get("filter[name]")
+            minPrice = self.request.query_params.get("filter[minPrice]")
+            maxPrice = self.request.query_params.get("filter[maxPrice]")
+            freeDelivery = self.request.query_params.get("filter[freeDelivery]")
+            available = self.request.query_params.get("filter[available]")
+            tags = self.request.query_params.getlist('tags[],[]')
+            print(self.request.query_params, "AAAAAAA")
+            if name:
+                queryset = queryset.filter(title__icontains=name)
+            if minPrice:
+                queryset = queryset.filter(price__gte=minPrice)
+            if maxPrice:
+                queryset = queryset.filter(price__lte=maxPrice)
+            if freeDelivery and freeDelivery == "true":
+                queryset = queryset.filter(freeDelivery=True)
+            if available == "true":
+                queryset = queryset.filter(available=True)
+            if len(tags) != 0:
+                queryset = queryset.filter(tags__in=tags)
+            return queryset
